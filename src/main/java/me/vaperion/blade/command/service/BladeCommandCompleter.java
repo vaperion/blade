0989d3b7bf04e7573a5444a690f6b11a5f6ce892
vaperion <vaperion@riseup.net>
2021-04-27 05:52:05 +0200

    Fixed NPE, added more debug

--- src/main/java/me/vaperion/blade/command/service/BladeCommandCompleter.java
+++ src/main/java/me/vaperion/blade/command/service/BladeCommandCompleter.java
@@ -11,7 +11,7 @@ import org.jetbrains.annotations.Nullable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 @RequiredArgsConstructor
 public class BladeCommandCompleter {
@@ -22,12 +22,19 @@ public class BladeCommandCompleter {
     public Tuple<BladeProvider<?>, String> getLastProvider(@NotNull BladeCommand command, @NotNull String[] args) throws BladeExitMessage {
         try {
             List<String> argumentList = new ArrayList<>(Arrays.asList(args));
+            System.out.println("Argument list: " + argumentList);
             List<String> arguments = command.isQuoted() ? commandService.getCommandParser().combineQuotedArguments(argumentList) : argumentList;
-            arguments.removeAll(commandService.getCommandParser().parseFlags(command, arguments).keySet().stream().map(String::valueOf).collect(Collectors.toList()));
+            System.out.println("Argument list 2: " + arguments);
 
-            System.out.println(" - Parameter providers: " + command.getParameterProviders().size() + " -> " + command.getParameterProviders());
-            System.out.println(" - Arguments: " + arguments.size() + " -> " + arguments);
+            Map<Character, String> flags = commandService.getCommandParser().parseFlags(command, arguments);
+            for (Map.Entry<Character, String> entry : flags.entrySet()) {
+                arguments.remove(String.valueOf(entry.getKey()));
+                if (!"true".equals(entry.getValue())) arguments.remove(entry.getValue());
+            }
 
+            System.out.println("Argument list 3: " + argumentList);
+
+            if (arguments.size() == 0) return new Tuple<>();
             if (command.getParameterProviders().size() < arguments.size()) return new Tuple<>();
 
             int index = Math.max(0, arguments.size() - 1);
