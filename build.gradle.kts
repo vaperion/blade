b2a32ec6762fe239f4651f265eb756ed964bf6a4
vaperion <vaperion@riseup.net>
2025-11-09 10:39:55 +0100

    Remove duplicate code

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "io.github.vaperion.blade"
-version = "1.0.1"
+version = "1.0.2"
 
 subprojects {
     apply(plugin = "io.freefair.lombok")
--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -9,5 +9,10 @@ dependencies {
     api(project(":brigadier"))
     api(project(":bukkit"))
 
+    api(project(":paper-legacy")) {
+        exclude(group = "io.papermc.paper", module = "paper-api")
+        exclude(group = "io.papermc.paper", module = "paper-mojangapi")
+    }
+
     compileOnly("io.papermc.paper:paper-api:${libs.versions.paper.new.get()}")
 }
